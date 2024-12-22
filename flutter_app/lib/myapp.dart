54ae8ad84dc74a66b04464507a4b23abca0a8602
neo-0007 <hrishikeshgohain123@gmail.com>
2024-12-22 22:06:03 +0530

    chore:setup folder structure
    - setup feature first folder structure
    - use mvvm architecture

---
+++ flutter_app/lib/myapp.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// Defines the main structure of the application, including the MaterialApp widget.
+// Handles the app's theme, routes, and overall configuration.
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
