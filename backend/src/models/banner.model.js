d4fa6c8069f1ca4d7ee99858efafd2f62aef4288
neo-0007 <hrishikeshgohain123@gmail.com>
2025-01-04 14:45:27 +0530

    feat: create banner api endpoints
    - banners are images shown in the homepage of applications
    - to-do: in add banner endpoint currently taking link has to take image
      upload it to R2 and store the link in db
    - to-do: add banner has to be root and admin access only

---
+++ backend/src/models/banner.model.js
@@ -0,0 +1,52 @@
+const BaseModel = require("./base.model");
+const mysql = require("mysql2");
+const config = require("../config/mysqlConfig");
+const pool = mysql.createPool(config);
+
+
+class Banner extends BaseModel {
+  constructor({institute_id,image_url, title, start_date, end_date,}) {
+    super("banners");
+    this.institute_id = institute_id;
+    this.image_url = image_url;
+    this.title = title;
+    this.start_date = start_date;
+    this.end_date = end_date;
+  }
+
+
+  static async getActiveBanners(date){
+    try {
+        const formattedDate = new Date(date).toISOString().slice(0, 19).replace("T", " ");
+
+        const query = `
+        SELECT * FROM banners
+        WHERE start_date <= ? AND end_date >= ?
+        `;
+
+        const [banners] = await pool.promise().query(query, [formattedDate, formattedDate]);
+
+        return banners;
+    } catch (error) {
+        throw new Error(`Failed to fetch active banners: ${error.message}`);
+    }
+  }
+
+  static async getActiveBannersOfInstitute(date,instituteId){
+    try {
+      const formattedDate = new Date(date).toISOString().slice(0, 19).replace("T", " ");
+      const query =`
+        SELECT * FROM banners
+        WHERE start_date <= ? AND end_date >= ? AND institute_id = ?
+        `;
+
+      const [banners] = await pool.promise().query(query,[formattedDate,formattedDate,instituteId]);
+
+      return banners;
+    } catch (error) {
+      throw new Error(`Failed to fetch active banners: ${error.message}`);
+    }
+  }
+}
+
+module.exports = Banner;
